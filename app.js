ad031140aa8ff46f9cc3bb4c7b35b45c02e0c864
dev-chava <salvador.mosqueda@certuslab.com.mx>
2025-05-01 23:25:51 -0700

    Update app.js
    actualice mi pagina

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('esto ya se levanto!!');
+  res.send('esto se actualizo 2.0!!');
 });
 
 var server = app.listen(3000, function () {
