f8b0175508563efe34772d35ebccee15ec32a630
dev-chava <salvador.mosqueda@certuslab.com.mx>
2025-07-07 10:58:20 -0700

    feat: :ambulance: s
    s
    
    s

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('esto se acual 50');
+  res.send('esto se acual! 50');
 });
 
 var server = app.listen(3000, function () {
